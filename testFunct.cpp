e49ebe4c26193dc1aaece38fde2282c8472ee732
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-08-28 16:40:48 +0300

    Delete testFunct.cpp

--- testFunct.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main() {
-    char s[100] = "x125x967897"; // calloc/malloc
-    char *st = {};
-    printf("%d\n",  strchr(st, 'p') - st);
-    
-    return 0;
-}
